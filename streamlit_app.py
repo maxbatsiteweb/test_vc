49af176ca1e783f34c53b9f1b0a0a450a98127e7
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-08-15 15:23:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,14 @@ st.write(
 receiver_email = st.text_input("Ton Email")
 st.write("The current movie title is", receiver_email)
 
+# Initialiser le nombre de champs dans session_state
+if 'input_count' not in st.session_state:
+    st.session_state.input_count = 3  # Par défaut, on commence avec 3 champs
+
+# Fonction pour ajouter un nouveau champ d'input
+def add_input():
+    st.session_state.input_count += 1
+
 
 race_1_hour = st.number_input("Heures", min_value=0, max_value=23, key="race_1_hour")
 race_1_min = st.number_input("Minutes", min_value=0, max_value=59, key="race_1_min")
@@ -21,6 +29,31 @@ st.write("Secondes", race_1_sec)
 race_2_hour = st.number_input("Heures", min_value=0, max_value=23, key="race_2_hour")
 race_2_min = st.number_input("Minutes", min_value=0, max_value=59, key="race_2_min")
 race_2_sec = st.number_input("Secondes", min_value=0, max_value=59, key="race_2_sec")
+
+
+
+### TEST
+
+if 'input_count' not in st.session_state:
+    st.session_state.input_count = 3  # Par défaut, on commence avec 3 champs
+
+# Fonction pour ajouter un nouveau champ d'input
+def add_input():
+    st.session_state.input_count += 1
+
+# Afficher les champs d'input
+st.write("Entrez les valeurs :")
+for i in range(st.session_state.input_count):
+    st.text_input(f'Input {i + 1}', key=f'input_{i}')
+
+# Bouton pour ajouter un nouveau champ d'input
+if st.button('Ajouter un champ'):
+    add_input()
+
+# Afficher les valeurs saisies
+st.write("Les valeurs saisies :")
+for i in range(st.session_state.input_count):
+    st.write(f"Input {i + 1}: {st.session_state[f'input_{i}']}")
 
 
 
