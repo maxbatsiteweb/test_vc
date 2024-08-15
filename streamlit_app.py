9949703f4ff32fd1c85fca832da37cc6d7213ba5
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-08-15 14:59:54 +0200

    Update streamlit_app.py
    step seconds

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,10 @@ receiver_email = st.text_input("Ton Email")
 st.write("The current movie title is", receiver_email)
 
 
-race_1 = st.time_input("Ton temps sur 10 000")
+race_1 = st.time_input("Ton temps sur 10 000", step=1)
 st.write("race_1", race_1)
 
-race_2 = st.time_input("Ton temps sur Semi")
+race_2 = st.time_input("Ton temps sur Semi", step=1)
 st.write("race_2", race_2)
 
 
