8c99190734c801ed3537d47bf50efb74fb71e170
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-08-15 17:19:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,18 +82,7 @@ v_fit = model(T_fit, E_opt, S_opt)
 plt.plot(T_fit, v_fit, color='red', label='Régression linéaire')
 
 
-# Distances en mètres
-distances_km = [5, 10, 21.1, 42.2]
-distances_m = np.array(distances_km) * 1000
 
-# Calcul du temps pour chaque distance avec les vitesses issues de la régression
-T_points = distances_m / v_fit[-1]  # Utiliser la vitesse correspondant à la fin de la courbe de régression
-v_points = model(T_points, E_opt, S_opt)
-
-# Tracer les points spécifiques sur la courbe de régression
-plt.scatter(T_points, v_points, color='green', label='Points spécifiques', zorder=5)
-for i, dist in enumerate(distances_km):
-    plt.text(T_points[i], v_points[i], f'{dist} km', fontsize=9, verticalalignment='bottom')
 
 
 
