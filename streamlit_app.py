af5d3a4c60675d27e8c7ca904dbe9126e9cd25ea
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-08-17 22:01:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,14 +155,15 @@ if total_seconds_1 > 0 and total_seconds_2 > 0:
             if receiver_email is None:
                 st.warning("Veuillez insérer une adresse mail.")
                 validation_status = False
-
-            # Regex pour vérifier le format string@string.
-            email_regex = r"^[^\s@]+@[^\s@]+\.[^\s@]+$"
-
-            if not re.match(email_regex, receiver_email):
-                st.warning("L'adresse mail n'est pas valide.")
-                validation_status = False
+            else:
                 
+                # Regex pour vérifier le format string@string.
+                email_regex = r"^[^\s@]+@[^\s@]+\.[^\s@]+$"
+                if not re.match(email_regex, receiver_email):
+                    st.warning("L'adresse mail n'est pas valide.")
+                    validation_status = False
+
+
             # Vérifier si la case est cochée
             if not checkbox:
                 # Afficher un message d'avertissement si la case n'est pas cochée
