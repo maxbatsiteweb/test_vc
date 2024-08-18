9ab91c2e5528e66d54d6958c840ca443721000f8
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2024-08-18 14:33:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,8 +211,8 @@ if total_seconds_1 > 0 and total_seconds_2 > 0:
                 """
     
                 subject = "An email with attachment from Python"
-                sender_email = "maximebataille95@gmail.com"
-                password = "upqm tezg vljv zhuh"
+                sender_email = "maximebataille.trailrunning@gmail.com"
+                password = "22a7u6kK!"
                 
                 # Create a multipart message and set headers
                 message = MIMEMultipart()
